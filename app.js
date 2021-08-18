d05fb85d34d8d8129b7cee3837305d9fa47da7ac
Sebastian Infante <sebastian.infante.dg@gmail.com>
2021-08-17 20:57:40 -0400

    Init proyect NodeJs with HBS

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set("view engine", "hbs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/generic", (req, res) => {
+  res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.get("/elements", (req, res) => {
+  res.sendFile(__dirname + "/public/elements.html");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
