220a165f67b67f996472bf8d612accc67c124ee1
Sebastian Infante <sebastian.infante.dg@gmail.com>
2021-08-19 21:10:23 -0400

    run to server

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const express = require("express");
-const app = express();
-const port = 8080;
+const hbs = require("hbs");
+require("dotenv").config();
 
+const app = express();
+const port = process.env.PORT;
+
+//Handlebars
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+// Carpeta contenido estatico
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+    res.render("home", {
+        nombre: "Sebastian Infante",
+        titulo: "Curso de Node",
+    });
 });
 
 app.get("/generic", (req, res) => {
-  res.sendFile(__dirname + "/public/generic.html");
+    res.render("generic", {
+        nombre: "Sebastian Infante",
+        titulo: "Curso de Node",
+    });
 });
 
 app.get("/elements", (req, res) => {
-  res.sendFile(__dirname + "/public/elements.html");
+    res.render("elements", {
+        nombre: "Sebastian Infante",
+        titulo: "Curso de Node",
+    });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
+    res.sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
